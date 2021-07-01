be220a2dad9875e30963969e56d2d2420c1cffc7
trongthinh <86757894+trongthinh@users.noreply.github.com>
2021-07-01 10:51:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Em rất thương anh <33',
-    btnNo: 'Không thương cũng phải thương :3',
+    btnNo: 'Không thương cũng phải thương hihi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
